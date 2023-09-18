93fcd75ddf05025f02abaf6e351b500a1b9662b8
Liango Liu <l.liu@gbh.com.do>
2023-09-18 16:06:25 -0400

    remove unnecessary params

--- src/graphql/mutations/createCast.js
+++ src/graphql/mutations/createCast.js
@@ -1,19 +1,15 @@
 module.exports = {
   generateMutation: () => `
     mutation CreateCast (
-      $organizationEid: String,
       $title: String,
       $file: Upload!,
       $isTemplate: Boolean,
-      $allowedAliasIds: [String],
       $detectFields: Boolean,
     ) {
       createCast (
-        organizationEid: $organizationEid,
         title: $title,
         file: $file,
         isTemplate: $isTemplate,
-        allowedAliasIds: $allowedAliasIds,
         detectFields: $detectFields,
       ) {
         versionNumber
